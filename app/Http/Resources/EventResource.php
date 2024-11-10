1e46a4301385b6b406e1ad75beac2298f46a98f5
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-11-10 21:55:28 +0100

    in AttendeeController.php/index() listed all the attendees from a event paginated

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -22,7 +22,10 @@ class EventResource extends JsonResource
             'description' => $this->description,
             'start_time' => $this->start_time,
             'end_time' => $this->end_time,
-            'user' => new UserResource($this->whenLoaded('user'))
+            'user' => new UserResource($this->whenLoaded('user')),
+            'attendees' => AttendeeResource::collection(
+                $this->whenLoaded('attendees')
+            )
             // 'created_at' => $this->created_at,
             // 'updated_at' => $this->updated_at,
         ];
